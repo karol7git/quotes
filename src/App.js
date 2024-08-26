f5faf3892ffc30f4aab94446c49fa5297388463d
karol7git <66336947+karol7git@users.noreply.github.com>
2024-08-26 12:30:56 -0500

    use axiso to get a quote

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useState } from "react";
 function App() {
+  const [quote, setQuote] = useState("");
+  const getQuote = () => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then(res => {
+        setQuote(res.data.content);
+      })
+      .catch(err => {
+        <p>{err} </p>;
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={getQuote}>get quote</button>
+      {quote && <p> {quote} </p> }
+    </>
   );
 }
 
